3fa15e7edf412e8a61ff1f8b06c3ea4f746e7f76
Madhur Ahuja <nicemadhur@gmail.com>
2015-01-20 19:19:25 -0800

    Update mebf.js

--- mebf.js
+++ mebf.js
@@ -1,46 +1,3 @@
-<!-- start Mixpanel -->
-(function(f, b) {
-if (!b.__SV) {
-var a, e, i, g;
-window.mixpanel = b;
-b._i = [];
-b.init = function(a, e, d) {
-    function f(b, h) {
-        var a = h.split(".");
-        2 == a.length && (b = b[a[0]], h = a[1]);
-        b[h] = function() {
-            b.push([h].concat(Array.prototype.slice.call(arguments, 0)))
-        }
-    }
-    var c = b;
-    "undefined" !== typeof d ? c = b[d] = [] : d = "mixpanel";
-    c.people = c.people || [];
-    c.toString = function(b) {
-        var a = "mixpanel";
-        "mixpanel" !== d && (a += "." + d);
-        b || (a += " (stub)");
-        return a
-    };
-    c.people.toString = function() {
-        return c.toString(1) + ".people (stub)"
-    };
-    i = "disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
-
-    for (g = 0; g < i.length; g++) f(c, i[g]);
-    b._i.push([a, e, d])
-};
-b.__SV = 1.2;
-a = f.createElement("script");
-a.type = "text/javascript";
-a.async = !0;
-a.src = "//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";
-e = f.getElementsByTagName("script")[0];
-e.parentNode.insertBefore(a, e)
-}
-})(document, window.mixpanel || []);
-
-mixpanel.init("0d56da8c63a83cc4ea6e70c369a98596");
-<!-- end Mixpanel -->
 
 //isDebugEnabled controls the entire site.
 var isDebugEnabled = true;
@@ -59,7 +16,7 @@ function chatButtonState(acct, skill) {
     var button = document.getElementById(acct + "-" + skill + "-chatButton");
 
     if (button) {
-        // //debugger;
+        // 
         if (LPMobile.getEnabled(acct, skill) == 1) {
             $(button).prop("disabled",false);
             $(button).removeClass("offline");
@@ -79,179 +36,103 @@ function chatButtonState(acct, skill) {
         console.log("in else", acct, skill);
     };
 }
-var getChannelIndex = function(elementText){
-    	var returningIndexValue;
-    	var checkboxes=$("input[type=checkbox]");
-    	for(i=0;i<checkboxes.length;i++){
-    		var eText=$(checkboxes[i]).siblings("span").text();
-    		if(eText==elementText){
-    			returningIndexValue=i;
-    			break;
-    		}
-    	}
-    	
-    	return returningIndexValue;
-    }
 
-var syncParamWithLP = function(element) {
-        debug("sync Param with LP called");
-        var elementText = $(element).siblings("span").text();
-        var index = getChannelIndex(elementText);
-        debug("ME_DisplayName_channels ------------------- start of section -------------------------")
-        debug("The Index of channel is - " + index + " & the elementText is - " + elementText);
-        if (element.checked) {
-            LPMobile.reportEvent("ME_DisplayName_channels " + index, elementText);
-            debug("--------------> Event for ME_DisplayName_channels " + index +" : "+elementText +" fired!")
-            debug("ME_DisplayName_channels " + index + " is set to " + elementText);
-            
-        } else {
-            LPMobile.reportEvent("ME_DisplayName_channels " + index, "-");
-            debug("--------------> Event for ME_DisplayName_channels " + index +" : Blank fired!")
-            debug("ME_DisplayName_channels " + index + " is set to 'Blank'. (The field name was-" + elementText+")");
-        }
-       
-        debug("ME_DisplayName_channels ------------------- end of section -------------------------")
-        
-    };
-
-var syncParamsWithLP = function(element) {
-        debug("sync Params with LP called");
-        $("input[type=checkbox]").each(function(index, element) {
-            var elementText = $(element).siblings("span").text();
-            if (element.checked) {
-                LPMobile.reportEvent("ME_DisplayName_channels " + index, elementText);
-                debug("ME_DisplayName_channels " + index + " is set to " + elementText);
-            } else {
-                LPMobile.reportEvent("ME_DisplayName_channels " + index, "-");
-                debug("ME_DisplayName_channels " + index + " is set to 'Blank'. (The field name was-" + elementText+")");
-            }
-        });
-        debug("ME_DisplayName_channels ------------------- end of section -------------------------")
-       
-    };
- var syncWithLP=function(element){
-     var approach=$('input[name=approach]:checked').val();
-     if(approach=="approach1"){
-         syncParamsWithLP(element); 
-     }else if(approach=="approach2"){
-         syncParamWithLP(element);
-     }
- }
 function onReady() {
     console.log("**********ONREADY*************");
     LPMobile.on("enabledChange", function (o) {
         chatButtonState(o.account, o.skill);
-        mixpanel.track("enabledChange")
+        console.log("enabledChange")
     });
 
     LPMobile.on('chatDisabled', function () {
-        mixpanel.track("chatDisabled")
+        console.log("chatDisabled")
     });
 
     LPMobile.on('chatEnabled', function () {
-        mixpanel.track("chatEnabled")
+        console.log("chatEnabled")
     });
 
     LPMobile.on('visitStart', function () {
-      mixpanel.track("visitStart")
+      console.log("visitStart")
     });
 
     LPMobile.on('hotLead', function () {
-      //debugger;
-        mixpanel.track("hotlead")
+        console.log("hotlead")
     });
 
     LPMobile.on('coldLead', function () {
-      //debugger;
-        mixpanel.track("coldLead")
+        console.log("coldLead")
     });
 
     LPMobile.on('invitationShow', function () {
-      //debugger;
-        mixpanel.track("invitationShow")
+        console.log("invitationShow")
     });
 
      /*For defect 7274*/
-            LPMobile.on('chatWindowShow', function () {
-               
-                $("body").attr("aria-hidden",true);
-                mixpanel.track("chatWindowShow")
-            });
+    LPMobile.on('chatWindowShow', function () {
+        $("body").attr("aria-hidden",true);
+        console.log("chatWindowShow")
+    });
             
-            LPMobile.on('chatWindowHide', function () {
-               
-                $("body").attr("aria-hidden",false);
-                mixpanel.track("chatWindowHide")
-            });
+    LPMobile.on('chatWindowHide', function () {
+        $("body").attr("aria-hidden",false);
+        console.log("chatWindowHide")
+    });
 
     LPMobile.on('windowShow', function () {
-      //debugger;
-        mixpanel.track("windowShow")
+        console.log("windowShow")
     });
 
     LPMobile.on('windowHide', function () {
-      //debugger;
-        mixpanel.track("windowHide")
+        console.log("windowHide")
     });
 
     LPMobile.on('visitorMessage', function () {
-      //debugger;
-        mixpanel.track("visitorMessage")
+        console.log("visitorMessage")
     });
 
     LPMobile.on('agentMessage', function () {
-      //debugger;
-        mixpanel.track("agentMessage")
+        console.log("agentMessage")
     });
 
     LPMobile.on('invitationAccept', function () {
-      //debugger;
-        mixpanel.track("invitationAccept")
+        console.log("invitationAccept")
     });
 
     LPMobile.on('chatEnd', function () {
-      //debugger;
-        mixpanel.track("chatEnd")
+        console.log("chatEnd")
     });
 
     LPMobile.on('chatInteractive', function () {
-      //debugger;
-        mixpanel.track("chatInteractive")
+        console.log("chatInteractive")
     });
 
     LPMobile.on('prechatSurveyCanceled', function () {
-      //debugger;
-        mixpanel.track("prechatSurveyCanceled")
+        console.log("prechatSurveyCanceled")
     });
 
     LPMobile.on('prechatSurveySubmit', function () {
-      //debugger;
-        mixpanel.track("prechatSurveySubmit")
+        console.log("prechatSurveySubmit")
     });
 
     LPMobile.on('offlineSurveySubmit', function () {
-      //debugger;
-        mixpanel.track("offlineSurveySubmit")
+        console.log("offlineSurveySubmit")
     });
 
     LPMobile.on('prechatSurveyShow', function () {
-      //debugger;
-        mixpanel.track("prechatSurveyShow")
+        console.log("prechatSurveyShow")
     });
 
     LPMobile.on('postchatSurveyShow', function () {
-      //debugger;
-        mixpanel.track("postchatSurveyShow")
+        console.log("postchatSurveyShow")
     });
 
     LPMobile.on('chatInteractive', function () {
-      //debugger;
-        mixpanel.track("chatInteractive")
+        console.log("chatInteractive")
     });
 
     LPMobile.on('offlineSurveyShow', function () {
-      //debugger;
-        mixpanel.track("offlineSurveyShow")
+        console.log("offlineSurveyShow")
     });
 
 
